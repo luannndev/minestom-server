f290288de57faf032bce6ab6d950b5114aeb2e28
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-20 17:22:16 +0200

    Initialize VelocityProxy or MojangAuth based on environment
    Added logic to initialize VelocityProxy if the VELOCITY_SECRET is provided; otherwise, MojangAuth is initialized. Also included logging for each service and started the Minecraft server with block handler registration for hanging signs.

--- src/main/java/dev/luan/server/server/Server.java
+++ src/main/java/dev/luan/server/server/Server.java
@@ -1,7 +1,12 @@
 package dev.luan.server.server;
 
+import dev.luan.server.blockhandler.HangingSignBlockHandler;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.extras.velocity.VelocityProxy;
+import net.minestom.server.utils.NamespaceID;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public final class Server {
@@ -20,6 +25,23 @@ public final class Server {
         this.log.accept("Loading enviroment.");
         var minecraftServer = MinecraftServer.init();
 
+        var velocitySecret = System.getProperty("VELOCITY_SECRET");
+        if (velocitySecret != null) {
+            this.log.accept("Enviroment: VelocityProxy[Minestom]");
+            this.log.accept("- " + VelocityProxy.PLAYER_INFO_CHANNEL);
+            VelocityProxy.enable(velocitySecret);
+        } else {
+            MojangAuth.init();
+            this.log.accept("Enviroment: MojangAuth[Minestom]");
+            this.log.accept("- " + MojangAuth.AUTH_URL);
+        }
+        this.log.accept("");
+
+        minecraftServer.start("0.0.0.0", 25565);
+
+        MinecraftServer.getBlockManager().registerHandler(NamespaceID.from("minecraft:hanging_sign"), HangingSignBlockHandler::new);
+
+        this.log.accept("Server started successfully. Took " + (System.currentTimeMillis() - startup) + "ms (" + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startup) + "s)");
         return minecraftServer;
     }
 }
