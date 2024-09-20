452a2dc85c4c43d871ae2a048be127c52a933494
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-20 17:16:13 +0200

    Add Gradle Shadow plugin and configure project dependencies
    Introduce the Gradle Shadow plugin for artifact shading. Update repositories and dependencies to include Minestom snapshots. Set Java compile options including source and target compatibility to Java 21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "dev.luan.server"
@@ -7,13 +8,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("net.minestom:minestom-snapshots:6fc64e3a5d")
+    testImplementation("net.minestom:minestom-snapshots:6fc64e3a5d")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    sourceCompatibility = JavaVersion.VERSION_21.toString()
+    targetCompatibility = JavaVersion.VERSION_21.toString()
 }
